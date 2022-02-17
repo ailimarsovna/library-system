28e5a9c97d572ef3a1e0a6207d35a3a10cbced00
Aili <ailimarsovna@gmail.com>
2022-02-17 20:52:55 +0600

    start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Delete\n2. Add\n3. Update");
+
+        int n = sc.nextInt();
+
+        switch (n) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+        }
     }
 }
