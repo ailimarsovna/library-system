d144a5c1b6d6d3034e89da84cd3b5ba58caa5ba9
Aili <ailimarsovna@gmail.com>
2022-02-23 17:58:32 +0600

    update

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -1,16 +1,16 @@
 package com.company;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Library {
     Connection c = null;
     Statement stmt = null;
+    ResultSet rs = null;
+    PreparedStatement ps = null;
     public void connect() {
         try {
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/library", "postgres", "6569");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/library", "postgres", "1724");
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName()+": " + e.getMessage());
@@ -18,14 +18,80 @@ public class Library {
         }
         System.out.println("DB opened successfully");
     }
-    public void addBook(int id, String name, String author, String genre, String description) {
+    public void addBook(int id, String name, String author, String genre) {
         try {
             stmt = c.createStatement();
             String sql = "INSERT INTO books VALUES(" + id + ", '"+ name +
-                    "', '" + author + "', '" + genre + "', '" + description + "')";
+                    "', '" + author + "', '" + genre + "')";
             stmt.executeUpdate(sql);
             stmt.close();
             c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName()+": "+ e.getMessage());
+            System.exit(0);
+        }
+    }
+    public void deleteBook(int id) {
+        try {
+            stmt = c.createStatement();
+            String sql = "DELETE FROM books WHERE id = " + id;
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName()+": "+ e.getMessage());
+            System.exit(0);
+        }
+    }
+    public void deleteBook(String name) {
+        try {
+            stmt = c.createStatement();
+            String sql = "DELETE FROM books WHERE name = '" + name + "'";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName()+": "+ e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public void showBookInfo(int id) {
+        try {
+            String sql = "SELECT * FROM books WHERE id = " + id;
+            ps = c.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+
+            System.out.println("id\t|\tname\t|\tauthor\t|\tgenre");
+            while (rs.next()) {
+
+                int id1 = rs.getInt("id");
+                String name = rs.getString("name");
+                String author = rs.getString("author");
+                String genre = rs.getString("genre");
+                System.out.println(id1 + "\t|\t" + name
+                        + "\t|\t" + author + "\t|\t" + genre);
+            }
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName()+": "+ e.getMessage());
+            System.exit(0);
+        }
+    }
+    public void showBookInfo(String name) {
+        try {
+            String sql = "SELECT * FROM books WHERE id = " + name;
+            stmt = c.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            int id1 = rs.getInt("id");
+//            String name = rs.getString("name");
+            String author = rs.getString("author");
+            String genre = rs.getString("genre");
+            System.out.println(id1 + "\t\t" + name
+                    + "\t\t" + author + "\t\t"
+                    + genre);
         } catch (Exception e) {
             System.err.println(e.getClass().getName()+": "+ e.getMessage());
             System.exit(0);
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,29 +4,39 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("1. Delete\n2. Add\n3. Update");
 
-        int n = sc.nextInt();
-
-        switch (n) {
-            case 1:
-                System.out.println("1");
-                break;
-            case 2:
-                System.out.println("2");
-                break;
-            case 3:
-                System.out.println("3");
-                break;
-        }
 
         Library library = new Library();
         library.connect();
-        library.addBook(1,"Ohota", "Haruki",
-                "Roman",
-                "«Opisanie");
+        int n = 0;
+        while (n != 4) {
+            System.out.println("1. Add new book\n2. Delete book\n3. Show book info\n4. Quit");
+            n = sc.nextInt();
+            switch (n) {
+                case 1:
+                    System.out.println("Insert id, name, author and genre with the Enter");
+                    library.addBook(sc.nextInt(), sc.next(), sc.next(), sc.next());
+                    break;
+                case 2:
+                    System.out.println("Insert book's id or name");
+                    String x = sc.next();
+                    if (Character.isDigit(x.charAt(0))) {
+                        library.deleteBook(Integer.parseInt(x));
+                    } else {
+                        library.deleteBook(x);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Insert book's id or name");
+                    String y = sc.next();
+                    if (Character.isDigit(y.charAt(0))) {
+                        library.showBookInfo(Integer.parseInt(y));
+                    } else {
+                        library.showBookInfo(y);
+                    }
+            }
+            library.connect();
+        }
     }
 }
