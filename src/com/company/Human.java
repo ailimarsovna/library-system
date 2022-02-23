beb4f7e91a2e3daee99fc2d478fdd1564f193c93
Aili <ailimarsovna@gmail.com>
2022-02-24 04:35:33 +0600

    Inheritance

---
+++ src/com/company/Human.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface Human {
+    public void showBooks();
+    public void showBookInfo(int id);
+    public void showBookInfo(String name);
+    public void connect();
+}
---
+++ src/com/company/Librarian.java
@@ -0,0 +1,99 @@
+package com.company;
+
+import java.sql.*;
+
+public class Librarian implements Human{
+
+    Connection c = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+    PreparedStatement ps = null;
+
+
+    @Override
+    public void connect() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/library", "postgres", "1724");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("DB opened successfully");
+    }
+
+    @Override
+    public void showBooks() {
+        try {
+            String sql = "SELECT * FROM books";
+            ps = c.prepareStatement(sql);
+            rs = ps.executeQuery();
+            System.out.println("id\t|\tname\t|\tauthor\t|\tgenre\t|\tdescription\t|\tavailability");
+            while (rs.next()) {
+                int id = rs.getInt("book_id");
+                String name = rs.getString("name");
+                String author = rs.getString("author");
+                String genre = rs.getString("genre");
+                String description = rs.getString("description");
+                String availability = rs.getString("availability");
+                System.out.println(id + "\t|\t" + name
+                        + "\t|\t" + author + "\t|\t" + genre + "\t|\t" + description+ "\t|\t" + availability);
+            }
+        }
+        catch(Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void showBookInfo(int id) {
+        try {
+            String sql = "SELECT * FROM books WHERE book_id = " + id;
+            ps = c.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+
+            System.out.println("id\t|\tname\t|\tauthor\t|\tgenre\t|\tdescription\t|\tavailability");
+            while (rs.next()) {
+
+                String name = rs.getString("name");
+                String author = rs.getString("author");
+                String genre = rs.getString("genre");
+                String description = rs.getString("description");
+                String availability = rs.getString("availability");
+                System.out.println(id + "\t|\t" + name
+                        + "\t|\t" + author + "\t|\t" + genre + "\t|\t" + description+ "\t|\t" + availability);
+            }
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void showBookInfo(String name) {
+        try {
+            String sql = "SELECT * FROM books WHERE name = '" + name + "'";
+            ps = c.prepareStatement(sql);
+            rs = ps.executeQuery();
+            System.out.println("id\t|\tname\t|\tauthor\t|\tgenre\t|\tdescription\t|\tavailability");
+            while (rs.next()) {
+
+                int id = rs.getInt("book_id");
+                String author = rs.getString("author");
+                String genre = rs.getString("genre");
+                String description = rs.getString("description");
+                String availability = rs.getString("availability");
+                System.out.println(id + "\t|\t" + name
+                        + "\t|\t" + author + "\t|\t" + genre + "\t|\t" + description+ "\t|\t" + availability);
+            }
+        }
+        catch(Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+}
