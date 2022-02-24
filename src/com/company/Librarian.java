b54ac018648e8e35bd2db80f4ec48b3813271b4e
aruissina <99401385+aruissina@users.noreply.github.com>
2022-02-24 19:58:03 +0600

    update librarian

--- src/com/company/Librarian.java
+++ src/com/company/Librarian.java
@@ -12,6 +12,7 @@ public class Librarian implements Human{
 
     @Override
     public void connect() {
+        // This method makes a connection to the database
         try {
             Class.forName("org.postgresql.Driver");
             c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/library", "postgres", "1724");
@@ -25,6 +26,7 @@ public class Librarian implements Human{
 
     @Override
     public void showBooks() {
+        // This method shows all books from the table
         try {
             String sql = "SELECT * FROM books";
             ps = c.prepareStatement(sql);
@@ -49,6 +51,7 @@ public class Librarian implements Human{
 
     @Override
     public void showBookInfo(int id) {
+        // this method shows one book by its id number
         try {
             String sql = "SELECT * FROM books WHERE book_id = " + id;
             ps = c.prepareStatement(sql);
@@ -75,6 +78,7 @@ public class Librarian implements Human{
 
     @Override
     public void showBookInfo(String name) {
+        // This method shows one book by its name
         try {
             String sql = "SELECT * FROM books WHERE name = '" + name + "'";
             ps = c.prepareStatement(sql);
@@ -96,43 +100,3 @@ public class Librarian implements Human{
             System.exit(0);
         }
     }
-    public void addBook(int id, String name, String author, String genre, String description) {
-        try {
-            stmt = c.createStatement();
-            String sql = "INSERT INTO books VALUES(" + id + ", '" + name +
-                    "', '" + author + "', '" + genre + "', '" + description + "', true" + ")";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-    }
-
-    public void deleteBook(int id) {
-        try {
-            stmt = c.createStatement();
-            String sql = "DELETE FROM books WHERE id = " + id;
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-    }
-
-    public void deleteBook(String name) {
-        try {
-            stmt = c.createStatement();
-            String sql = "DELETE FROM books WHERE name = '" + name + "'";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-    }
-}
